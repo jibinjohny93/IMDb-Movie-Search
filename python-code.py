fcb0cc97cb07c53bc0cfcc138499bd6d32956e49
jibinjohny <jibinjohny815@gmail.com>
2024-06-12 20:56:03 +0200

    basic import info added

--- python-code.py
+++ python-code.py
@@ -1,5 +1,19 @@
-import IMDbPY
+from imdb import Cinemagoer
+
 import pandas
 import numpy
 import boto3
+
+
+
+print("-----------------------------------------------------------------------------------------")
+
+movie=input("Enter your full movie name: ")
+movie=Cinemagoer.parse(movie)
+
+
+
+print("-----------------------------------------")
+print("Please find the movie informaion below,")
+
 
