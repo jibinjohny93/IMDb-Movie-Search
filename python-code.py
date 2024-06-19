7d2cf1d2c79a57533126d6679137afb48188b9cb
jibinjohny <jibinjohny815@gmail.com>
2024-06-19 20:43:45 +0200

    new updates this week

--- python-code.py
+++ python-code.py
@@ -1,19 +1,39 @@
-from imdb import Cinemagoer
-
 import pandas
 import numpy
 import boto3
+from imdb import Cinemagoer
 
-
+# Create an instance of the Cinemagoer class
+ia = Cinemagoer()
 
 print("-----------------------------------------------------------------------------------------")
 
-movie=input("Enter your full movie name: ")
-movie=Cinemagoer.parse(movie)
+# Get the movie name from the user
+movie_name = input("Enter the full movie name: ")
 
+# Search for the movie
+movies = ia.search_movie(movie_name)
+if movies:
+    # Get the first movie (you can adjust this based on your requirements)
+    movie_id = movies[0].movieID
+    movie_info = ia.get_movie(movie_id, info=['main'])
 
+    # Extract and format the names of the people involved
+    def get_names(person_list):
+        return [person['name'] for person in person_list] if person_list else 'N/A'
+    
+    # Print relevant information
+    print(f"Title: {movie_info.get('title')}")
+    print(f"IMDb Rating: {movie_info.get('rating')}")
+    print(f"Released Year: {movie_info.get('year')}")
+    print(f"Genre: {movie_info.get('genre')}")
+    print(f"Language: {movie_info.get('language')}")
+    print(f"Directors: {', '.join(get_names(movie_info.get('directors')))}")
+    print(f"Producer: {', '.join(get_names(movie_info.get('producer')))}")
+    print(f"Cast: {', '.join(get_names(movie_info.get('cast')[:5]))}")
+    print(f"Writer(s): {', '.join(get_names(movie_info.get('writers')))}")
 
-print("-----------------------------------------")
-print("Please find the movie informaion below,")
+else:
+    print(f"Sorry, No movie found for '{movie_name}', the given name is either incorrect nor it does not have sufficient information...")
 
-
+print("-----------------------------------------------------------------------------------------")
